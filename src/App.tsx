bdfe2d1f6040376e5a1eaed2b13b63c2a4c18ff3
hurairah97 <abuhurairahkhan.97@gmail.com>
2025-08-05 17:29:41 +0500

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-// import Routing from './router/Index'
+import Routing from './router/Index'
 
 function App() {
 
   return (
     <div>
-      <div className='text-4xl'>test</div>
-      {/* <Routing /> */}
+      <Routing />
     </div>
   )
 }
--- src/router/Index.tsx
+++ src/router/Index.tsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import { Clients, Home } from '../pages'
+import React, { type JSX } from "react";
+import { Route, Routes } from "react-router";
+import { Clients, Home } from "../pages";
+import SignIn from "../pages/SignIn";
+
+interface Route {
+  path: string;
+  component: JSX.Element;
+}
 
 const Routing = React.memo(() => {
-  return (
-    <div className='mx-3 text-lightBlackText font-jakarta'>
-      <Routes>
-        <Route path="/" element={<Home />} />  
-        <Route path="/clients" element={<Clients />} />  
-      </Routes>
-    </div>
-  )
-})
+  const routes: Route[] = [
+    {
+      path: "/",
+      component: <Home />,
+    },
+    {
+      path: "/clients",
+      component: <Clients />,
+    },
+  ];
 
-export default Routing
+  const token: boolean = false;
+
+  return (
+    <div>
+      {token ? (
+      <Routes>
+        {routes.map(({ path, component }) => (
+          <>
+            {/* // <React.Fragment index={path}> */}
+            <Route path={path} element={component} />
+            {/* </React.Fragment> */}
+          </>
+        ))}
+      </Routes>
+      ) : <SignIn/>}
+    </div>
+  );
+});
+
+export default Routing;
