e5ed90d433a7d76da8ae035e170a309b20d23ca0
hurairah97 <abuhurairahkhan.97@gmail.com>
2025-08-05 20:58:25 +0500

    feat: add images from figma design in image folder

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import largePaw from './largePaw.png'
+import midPaw from './midPaw.png'
+import smallPaw from './smallPaw.png'
+import logo from './logo.png'
+
+export { largePaw, midPaw, smallPaw,logo }
