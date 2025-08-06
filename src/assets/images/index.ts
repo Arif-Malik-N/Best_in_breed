3f7cce9e9e3ed6b43d0fdba9a3cc6451f595db02
hurairah97 <abuhurairahkhan.97@gmail.com>
2025-08-06 15:49:35 +0500

    chore: add images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,8 @@
-import largePaw from './largePaw.png'
-import midPaw from './midPaw.png'
-import smallPaw from './smallPaw.png'
-import logo from './logo.png'
+import largePaw from "./largePaw.png";
+import midPaw from "./midPaw.png";
+import smallPaw from "./smallPaw.png";
+import logo from "./logo.png";
+import eyeSlashIcon from "./eyeSlashIcon.png";
+import unlock1 from "./unlock1.png";
 
-export { largePaw, midPaw, smallPaw,logo }
+export { largePaw, midPaw, smallPaw, logo, eyeSlashIcon, unlock1 };
