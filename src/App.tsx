7a798c2d124b7aabc7ec9c3794eb8ba8850c9f72
hurairah97 <abuhurairahkhan.97@gmail.com>
2025-08-20 14:35:39 +0500

    chore: changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Routing from './router/Index'
+import "./App.css";
+import Routing from "./router/Index";
 
 function App() {
-
   return (
     <div>
       <Routing />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
