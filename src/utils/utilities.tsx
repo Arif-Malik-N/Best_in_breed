444bc35fdee5bfb488117c5adc8edba01ef95f82
hurairah97 <abuhurairahkhan.97@gmail.com>
2025-10-08 16:35:47 +0500

    feat: commit extra field from required field array

--- src/utils/utilities.tsx
+++ src/utils/utilities.tsx
@@ -69,5 +69,7 @@ export const addressRegex = /^\d+\s[A-z]+\s[A-z]+.*$/;
 export const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 export const mailingAddressRegex =
-  /^$|^[A-Za-z0-9\s.,#\-]+(?:\n?[A-Za-z0-9\s.,#\-]+)*,\s*[A-Za-z\s]+,\s*[A-Z]{2}\s*\d{5}(?:-\d{4})?$/;
+  /^(?:\d+\s[A-Za-z0-9\s.#,]+(?:\s[A-Za-z]+)?,\s[A-Za-z\s]+,\s[A-Z]{2}\s\d{5}(?:-\d{4})?)$/;
+// export const mailingAddressRegex =
+//   /^$|^[A-Za-z0-9\s.,#\-]+(?:\n?[A-Za-z0-9\s.,#\-]+)*,\s*[A-Za-z\s]+,\s*[A-Z]{2}\s*\d{5}(?:-\d{4})?$/;
 // /^[a-zA-Z0-9\s,.\-#]+(?:\s+[A-Z]{2}\s+\d{5}(?:-\d{4})?)?$/;
